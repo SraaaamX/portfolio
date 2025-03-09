16f848c1c0c5317a93d5ff212620d4c72d5fbaab
SraaaamX <julesl1107@gmail.com>
2025-03-09 19:48:42 +0100

    Added autoprefixer / postcss to the project

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['> 1%', 'last 10000000000000000 versions', 'not dead'],
+        })
+      ],
+    },
+  },
 });
