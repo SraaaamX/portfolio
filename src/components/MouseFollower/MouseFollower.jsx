233f1f7ef4b7d7001d1dc56bd5c63362ac916ce7
SraaaamX <julesl1107@gmail.com>
2025-03-11 18:43:32 +0100

    Mise à jour des composants et styles : - Modifié : `src/components/Banner/Banner.module.sass` pour améliorer le style de la bannière. - Modifié : `src/components/Footer/Footer.jsx` pour ajuster le footer. - Modifié : `src/components/MouseFollower/MouseFollower.jsx` pour améliorer l'interactivité. - Modifié : `src/components/UpArrow/UpArrow.jsx` et `src/components/UpArrow/UpArrow.module.sass` pour optimiser l'affichage du bouton de retour en haut.

--- src/components/MouseFollower/MouseFollower.jsx
+++ src/components/MouseFollower/MouseFollower.jsx
@@ -3,6 +3,7 @@ import styles from './MouseFollower.module.sass';
 
 export const MouseFollower = () => {
     const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
     const updatePosition = useCallback((e) => {
         requestAnimationFrame(() => {
@@ -12,8 +13,18 @@ export const MouseFollower = () => {
 
     useEffect(() => {
         window.addEventListener('mousemove', updatePosition);
-        return () => window.removeEventListener('mousemove', updatePosition);
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('mousemove', updatePosition);
+            window.removeEventListener('resize', handleResize);
+        };
     }, [updatePosition]);
+
+    if (windowWidth <= 1024) {
+        return null;
+    }
 
     return (
         <div
