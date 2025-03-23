1679295fa646e296c4fb5c185c7e815c94b29e8c
SraaaamX <julesl1107@gmail.com>
2025-03-23 19:59:43 +0100

    Resorted icons in HardSkillsIcons.jsx

--- src/components/Skills/HardSkills/HardSkillsIcons/HardSkillsIcons.jsx
+++ src/components/Skills/HardSkills/HardSkillsIcons/HardSkillsIcons.jsx
@@ -3,34 +3,34 @@ import ScrollAnimation from '../../../Animation/ScrollAnimation';
 import styles from './HardSkillsIcons.module.sass';
 import htmlIcon from '../../../../assets/images/skills/html.webp';
 import cssIcon from '../../../../assets/images/skills/css.webp';
+import sassIcon from '../../../../assets/images/skills/sass.webp';
 import jsIcon from '../../../../assets/images/skills/js.webp';
 import phpIcon from '../../../../assets/images/skills/php.webp';
 import sqlIcon from '../../../../assets/images/skills/sql.webp';
-import sassIcon from '../../../../assets/images/skills/sass.webp';
+import pythonIcon from '../../../../assets/images/skills/python.webp';
 import reactIcon from '../../../../assets/images/skills/react.webp';
 import vueIcon from '../../../../assets/images/skills/vue.webp';
 import angularIcon from '../../../../assets/images/skills/angular.webp';
 import laravelIcon from '../../../../assets/images/skills/laravel.webp';
 import wordpressIcon from '../../../../assets/images/skills/wordpress.webp';
-import pythonIcon from '../../../../assets/images/skills/python.webp';
-import gitIcon from '../../../../assets/images/skills/git.webp';
 import figmaIcon from '../../../../assets/images/skills/figma.webp';
+import gitIcon from '../../../../assets/images/skills/git.webp';
 
 const icons = [
     { src: htmlIcon, alt: 'HTML' },
     { src: cssIcon, alt: 'CSS' },
+    { src: sassIcon, alt: 'Sass' },
     { src: jsIcon, alt: 'JavaScript' },
     { src: phpIcon, alt: 'PHP' },
     { src: sqlIcon, alt: 'SQL' },
-    { src: sassIcon, alt: 'Sass' },
+    { src: pythonIcon, alt: 'Python' },
     { src: reactIcon, alt: 'React' },
     { src: vueIcon, alt: 'Vue' },
     { src: angularIcon, alt: 'Angular' },
     { src: laravelIcon, alt: 'Laravel' },
     { src: wordpressIcon, alt: 'WordPress' },
-    { src: pythonIcon, alt: 'Python' },
-    { src: gitIcon, alt: 'Git' },
     { src: figmaIcon, alt: 'Figma' },
+    { src: gitIcon, alt: 'Git' },
 ];
 
 const HardSkillsIcons = () => {
