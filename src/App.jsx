ea14ffe45b550e75134f1f2eaf27d6618184ccad
SraaaamX <julesl1107@gmail.com>
2025-03-23 13:47:41 +0100

    Added Lenis library for smooth scroll to the website in order to improve the user experience by providing a seamless scrolling experience.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.sass';
+import { ReactLenis } from 'lenis/react';
 import Header from './components/Header/Header';
 import Banner from './components/Banner/Banner';
 import MouseFollower from './components/MouseFollower/MouseFollower';
@@ -12,25 +13,29 @@ import Projects from './components/Projects/Projects';
 
 function App() {
   return (
-    <div className="App">
-      <MouseFollower />
-      <Header />
-      <Banner />
-      <main>
-        <UpArrow />
-        <Separator id="about-container" />
-        <About />
-        <Separator id="soft-skills-container" />
-        <SoftSkills />
-        <Separator id="hard-skills-container" />
-        <HardSkills />
-        <Separator id="projects-container" />
-        <Projects />
-      </main>
-      <Footer />
-    </div>
+    <ReactLenis root>
+      <div className="App">
+        <MouseFollower />
+        <Header />
+        <Banner />
+        <main>
+          <UpArrow />
+          <Separator id="about-container" />
+          <About />
+          <Separator id="soft-skills-container" />
+          <SoftSkills />
+          <Separator id="hard-skills-container" />
+          <HardSkills />
+          <Separator id="projects-container" />
+          <Projects />
+        </main>
+        <Footer />
+      </div>
+    </ReactLenis>
   );
 }
 
 export default App;
+
+
 
